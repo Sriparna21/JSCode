18b013f4c1dd04f4aab9630f6ca8c730d0c7a9a7
Sriparna21 <sriparnagc21@gmail.com>
2025-08-25 03:46:55 +0000

    File describing datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all the JS code as newer version
+
+let name = "Srips" //string
+let age = 18 // number - normal numbers
+//bigint agar bahut hi bada number hai
+
+let loggedIn = false // boolean - true/false
+// null : standalone value
+// undefined : no value defined
+// null and undefined are different. Null strings are empty and undefined is where values are not defined
+// symbol : defines components as unique
+
+// object
+
+
+console.log (typeof "??")
+
+console.log (typeof undefined) // Result: undefined
+
+console.log(typeof null) // Result: object
