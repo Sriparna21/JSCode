08a976a82031a799ca83d781a456ebb4412711d6
Sriparna21 <sriparnagc21@gmail.com>
2025-08-25 03:21:04 +0000

    Variables lesson commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 562671
+let accountEmail = "sgc@gmail.com"
+var accountPassword = "sgc123"
+accountCity = "Delhi"
+let accountState;
+
+
+// Trying to change the variables for variables
+accountEmail = "abc@gmail.com"
+accountPassword = "7861"
+accountCity = "Mumbai"
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
+// Trying to change the variables for variables
+accountEmail = "a1245@gmail.com"
+accountPassword = "7866654"
+accountCity = "Hyderabad"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
