b9301074e14c268fd3f0ad0912272bc6a379c5b6
Sriparna21 <sriparnagc21@gmail.com>
2025-08-26 00:22:49 +0000

    Adding notes for dates

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,9 +4,28 @@ console.log (`Hello my name is ${name}`)
 
 const gameName = new String ('Sriparna')
 
-console.log (gameName[2])
-console.log (gameName.__proto__)
-console.log (gameName.length)
-console.log (gameName.toUpperCase())
-console.log (gameName.charAt(6))
-console.log (gameName.indexOf('a'))
+// console.log (gameName[2])
+// console.log (gameName.__proto__)
+// console.log (gameName.length)
+// console.log (gameName.toUpperCase())
+// console.log (gameName.charAt(6))
+// console.log (gameName.indexOf('a'))
+
+
+// Dates
+
+let myDate = new Date()
+// console.log('The date today is ' + myDate.toDateString())
+
+let myCreatedDate = new Date (2023, 12, 23)
+// console.log('The date today is ' + myCreatedDate.toDateString())
+
+let tsNow = Date.now()
+// console.log(tsNow)
+
+console.log(myCreatedDate.toLocaleDateString ('default',{
+    weekday: "long"
+}
+
+))
+
